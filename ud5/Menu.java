f21dbacfc67130ee8823bcd30cef50ae7bcc8ed3
Julio Jesús León Pérez <xulioxesus@gmail.com>
2023-10-23 10:15:10 +0200

    Clase Menu

---
+++ ud5/Menu.java
@@ -0,0 +1,36 @@
+package CD2023.ud5;
+
+import java.util.Scanner;
+
+public class Menu {
+    private int seleccion = -1;
+
+    public int getSeleccion() {
+        return seleccion;
+    }
+
+    private void imprimir() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Seleccione una opción");
+        System.out.println("0 - Reset");
+        System.out.println("1 - Sumar");
+        System.out.println("2 - Restar");
+        System.out.println("3 - Multiplicar");
+        System.out.println("4 - Dividir");
+
+        int valorLeido = sc.nextInt();
+
+        if (valorLeido >= 0 && valorLeido <= 4) {
+            this.seleccion = valorLeido;
+        } else {
+            this.seleccion = -1;
+        }
+    }
+
+    public int getSeleccionValida() {
+        while (seleccion == -1){
+            imprimir();
+        }
+        return seleccion;
+    }
+}
---
+++ ud5/MenuTest.java
@@ -0,0 +1,9 @@
+package CD2023.ud5;
+
+public class MenuTest {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.getSeleccionValida();
+        System.out.println("Opción: " + menu.getSeleccion());
+    }
+}
